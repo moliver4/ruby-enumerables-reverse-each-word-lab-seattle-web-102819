1ae560e7062e958f1ee137fec59b149a544c3e6f
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 20:38:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   array1= sentence1.split(" ")
   new_string=[]
   array1.each do |word|
-    new_string.push(word.reverse})
+    new_string.push("#{word.reverse}"")
   end
   new_string.join(" ")
 end
