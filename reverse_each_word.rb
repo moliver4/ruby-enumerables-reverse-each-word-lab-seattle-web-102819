9a930dcdfe28727ef97e83e48bd252b66d6d5df8
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 20:34:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(string)
+def reverse_each_word(sentence1)
   new_string= []
-  %w[string].each do |word|
+  %w[sentence1].each do |word|
     new_string.push("#{word.reverse}")
   end
   new_string.join(" ")
